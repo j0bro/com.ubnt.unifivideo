0e596d51f8d974b5552f4023d312e55b2c13b07b
Jeroen Brosens <jeroen.brosens@philips.com>
2020-01-02 01:32:33 +0100

    Fixed eslint warnings, added events to test

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 'use strict';
 
 const Homey = require('homey');
@@ -5,35 +7,34 @@ const UfvApi = require('./lib/ufvapi');
 const UfvConstants = require('./lib/ufvconstants');
 
 class UniFiVideo extends Homey.App {
+  onInit() {
+    this.api = new UfvApi();
 
-    onInit() {
-        this.api = new UfvApi();
+    this.api.on(UfvConstants.API_HOST, apihost => {
+      console.log('Saving API host in settings.');
+      Homey.ManagerSettings.set(UfvConstants.API_HOST, apihost);
+    });
 
-        this.api.on(UfvConstants.API_HOST, apihost => {
-            console.log('Saving API host in settings.');
-            apihost.ip
-            Homey.ManagerSettings.set(UfvConstants.API_HOST, apihost);
-        });
+    this.api.on(UfvConstants.API_KEY, apikey => {
+      console.log('Saving API key in settings.');
+      Homey.ManagerSettings.set(UfvConstants.API_KEY, apikey);
+    });
 
-        this.api.on(UfvConstants.API_KEY, apikey => {
-            console.log('Saving API key in settings.');
-            Homey.ManagerSettings.set(UfvConstants.API_KEY, apikey);
-        });
+    this.api.Discover();
 
-        this.api.Discover();
+    this.snapshotToken = new Homey.FlowToken('ufv_snapshot', {
+      type: 'image',
+      title: 'Snapshot',
+    });
 
-        this.snapshotToken = new Homey.FlowToken('ufv_snapshot', {
-            type: 'image',
-            title: 'Snapshot'
-        });
+    Homey.ManagerFlow.registerToken(this.snapshotToken);
 
-        Homey.ManagerFlow.registerToken(this.snapshotToken);
-
-        if (Homey.env.DEBUG) {
-            require('inspector').open(9229, '0.0.0.0');
-        }
-        this.log('[APP] UniFi Video is running.');
+    if (Homey.env.DEBUG) {
+      // eslint-disable-next-line global-require
+      require('inspector').open(9229, '0.0.0.0');
     }
+    this.log('[APP] UniFi Video is running.');
+  }
 }
 
 module.exports = UniFiVideo;
